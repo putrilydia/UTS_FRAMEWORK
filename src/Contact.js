b09bb2b8624adddeeb4cd7c437b1e3ee367bd4a9
putrilydia <75605549+putrilydia@users.noreply.github.com>
2022-06-10 15:48:02 +0700

    DONE ALL

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Email : BStore@gmail.com <br />
+           Phone : 083108514500
+</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
